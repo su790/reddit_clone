8130141565109a1c2b4170cb779a45361a0514a1
su790 <jungthapasushant@gmail.com>
2023-01-23 21:27:39 +0545

    removed pages in nextauth

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -1,21 +1,42 @@
+import { prisma } from "./../../db";
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure, protectedProcedure } from "../trpc";
 
-export const exampleRouter = createTRPCRouter({
-  hello: publicProcedure
-    .input(z.object({ text: z.string() }))
-    .query(({ input }) => {
-      return {
-        greeting: `Hello ${input.text}`,
-      };
+export const userRouter = createTRPCRouter({
+  create: protectedProcedure
+    .input(
+      z.object({
+        username: z.string(),
+        email: z.string(),
+        avatar: z.string(),
+        display_name: z.string(),
+        banner_image: z.string(),
+        userId: z.string(),
+      })
+    )
+    .mutation(({ input }) => {
+      prisma.profile.create({
+        data: { ...input },
+      });
     }),
-
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.example.findMany();
-  }),
-
-  getSecretMessage: protectedProcedure.query(() => {
-    return "you can now see this secret message!";
-  }),
+  update: protectedProcedure
+    .input(
+      z.object({
+        username: z.string().optional(),
+        email: z.string().optional(),
+        avatar: z.string().optional(),
+        display_name: z.string().optional(),
+        banner_image: z.string().optional(),
+        userId: z.string(),
+      })
+    )
+    .mutation(({ input }) => {
+      prisma.profile.update({
+        where: {
+          id: input.userId,
+        },
+        data: { ...input },
+      });
+    }),
 });
