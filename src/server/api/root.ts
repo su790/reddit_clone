404f33a3b782cd924bb57f389ea2fcf8d0cdc5f4
su790 <jungthapasushant@gmail.com>
2023-01-23 21:42:14 +0545

    Fixed root.ts

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,5 @@
 import { createTRPCRouter } from "./trpc";
-import { exampleRouter } from "./routers/user";
+import { userRouter } from "./routers/user";
 
 /**
  * This is the primary router for your server.
@@ -7,7 +7,7 @@ import { exampleRouter } from "./routers/user";
  * All routers added in /api/routers should be manually added here
  */
 export const appRouter = createTRPCRouter({
-  user: exampleRouter,
+  user: userRouter,
 });
 
 // export type definition of API
