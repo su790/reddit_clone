f0bd80181b34bb529773df8db85a30dd652a3ced
Bikrant Jung <bikrantjung1214@gmail.com>
2023-01-23 21:28:59 +0545

    Initial login page + added pages in next-auth

---
+++ src/pages/login.tsx
@@ -0,0 +1,23 @@
+import { signOut, signIn, useSession } from "next-auth/react";
+import React from "react";
+
+function Login() {
+  const { data: sessionData } = useSession();
+  return (
+    <div>
+      <button
+        className="m-2 rounded-full bg-primary-700 px-3 py-2 font-sans"
+        onClick={
+          sessionData
+            ? () => void signOut()
+            : () =>
+                void signIn("google", { callbackUrl: "http://localhost:3000" })
+        }
+      >
+        {sessionData ? "Sign out" : "Sign in"}
+      </button>
+    </div>
+  );
+}
+
+export default Login;
