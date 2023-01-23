33e0a1d86bd58e366679bc622d8bbe2fe0009cc5
Bikrant Jung <bikrantjung1214@gmail.com>
2023-01-23 11:45:33 +0545

    Custom Fonts

---
+++ src/styles/customFonts.ts
@@ -0,0 +1,20 @@
+import { IBM_Plex_Sans, Noto_Sans, IBM_Plex_Serif } from "@next/font/google";
+const IBMPlexSans = IBM_Plex_Sans({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700"],
+  fallback: ["sans"],
+});
+const NotoSans = Noto_Sans({ weight: "400" });
+const IBMPlexSerif = IBM_Plex_Serif({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700"],
+  fallback: ["serif"],
+});
+const root = `
+  :root {
+    --heading-font: ${IBMPlexSans.style.fontFamily};
+    --sans-font: ${NotoSans.style.fontFamily};
+    --serif-font:${IBMPlexSerif.style.fontFamily};
+  }
+  `;
+export { root };
